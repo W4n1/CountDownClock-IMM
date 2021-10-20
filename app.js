df490cff23894cb903b9b984f164df7dc6896ce0
W4n1 <wanikalimira@hotmail.co.uk>
2021-10-20 18:15:29 +0000

    adding JS script to make the clock count down

---
+++ app.js
@@ -0,0 +1,6 @@
+const countDate = () => {
+    const countDate = new Date("October 17, 2022 00:00:00"); //inserted is the date we are counting down to, this can be changed 
+    console.log(countDate);
+};
+
+countDate();
