31fa91b981855678b8819f7a3a9a6fa7e84f8e58
W4n1 <wanikalimira@hotmail.co.uk>
2021-10-21 22:18:20 +0000

    connecting HTML to the JS so that the data is connected to both files using .queryselector and innerText

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const countDate = () => {
-    const countDate = new Date("October 20, 2021 00:00:00").getTime();  //inserted is the date we are counting down to, this can be changed 
+    const countDate = new Date("December 25, 2021 00:00:00").getTime();  //inserted is the date we are counting down to, this can be changed 
     const now = new Date().getTime();
     const gap = countDate -  now;
     
@@ -11,11 +11,18 @@ const countDate = () => {
     const day = hour * 24;
     
     // calculation
-    const textDay = Math.floor(gap / day) ;
-    const textHour = Math.floor((gap % day) / hour) ;  // this sign % means a remainder of a value
-    const textMinute = Math.floor((gap % hour) / minute) ;
-    const textSecond = Math.floor((gap % day) / hour) ;
+    const textDay = Math.floor(gap / day);   // math.floor rounds a digit to the nearest number
+    const textHour = Math.floor((gap % day) / hour);  // this sign % means a remainder of a value
+    const textMinute = Math.floor((gap % hour) / minute);
+    const textSecond = Math.floor((gap % minute) / second);
+
+    //updating HTML
+    document.querySelector('.day'). innerText = textDay;
+    document.querySelector('.hour'). innerText = textHour;
+    document.querySelector('.minute'). innerText = textMinute;
+    document.querySelector('.second'). innerText = textSecond;
+
+
 };
 
-console.log(20 % 2);
 countDate();
