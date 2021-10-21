c7c96a7bba81b55f070e08178c63c733d45f9066
W4n1 <wanikalimira@hotmail.co.uk>
2021-10-21 21:20:49 +0000

    adding the date to be counted down from

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const countDate = () => {
-    
-};
+    const countDate = new Date("October 20, 2021 00:00:00").getTime();  //inserted is the date we are counting down to, this can be changed 
+ 
 
 countDate();
