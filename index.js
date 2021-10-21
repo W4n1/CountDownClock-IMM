26fa0546ac71e66dfbf16485f3e68af3d657b777
W4n1 <wanikalimira@hotmail.co.uk>
2021-10-21 21:28:37 +0000

    used getTime to measure the time in milliseconds, got the current time and the substracting it o get the difference

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const countDate = () => {
     const countDate = new Date("October 20, 2021 00:00:00").getTime();  //inserted is the date we are counting down to, this can be changed 
- 
+    const now = new Date (). getTime();
+    const gap = countDate -  now;
+    
+    
+};
 
 countDate();
